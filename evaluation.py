1a3e4950ec5ce886b7c919ef185715e78ce31e16
linna <limsolinna@gmail.com>
2026-06-25 17:03:57 +0700

    model evaluation
    Added predictions and accuracy calculations for Decision Tree, Random Forest, and SVM models. Included classification report for Random Forest model.

--- evaluation.py
+++ evaluation.py
@@ -1,1 +1,17 @@
 from sklearn.metrics import accuracy_score, classification_report
+from features import X_test, y_test
+from training import dt, rf, svm
+
+dt_pred = dt.predict(X_test)
+rf_pred = rf.predict(X_test)
+svm_pred = svm.predict(X_test)
+
+dt_acc = accuracy_score(y_test, dt_pred)
+rf_acc = accuracy_score(y_test, rf_pred)
+svm_acc = accuracy_score(y_test, svm_pred)
+
+print(f"Decision Tree accuracy is: ", round(dt_acc * 100, 2), "%")
+print(f"Random Forest accuracy is: ", round(rf_acc * 100, 2), "%")
+print(f"SVM accuracy is: ", round(svm_acc * 100, 2), "%")
+
+print(classification_report(y_test, rf_pred))
