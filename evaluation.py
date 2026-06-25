ce500e6ce14fd739d3d1686ce0656eac8ef092f9
linna <limsolinna@gmail.com>
2026-06-25 17:09:51 +0700

    evaluation updated

--- evaluation.py
+++ evaluation.py
@@ -10,8 +10,8 @@ dt_acc = accuracy_score(y_test, dt_pred)
 rf_acc = accuracy_score(y_test, rf_pred)
 svm_acc = accuracy_score(y_test, svm_pred)
 
-print(f"Decision Tree accuracy is: ", round(dt_acc * 100, 2), "%")
-print(f"Random Forest accuracy is: ", round(rf_acc * 100, 2), "%")
-print(f"SVM accuracy is: ", round(svm_acc * 100, 2), "%")
+print(f"Decision Tree accuracy is: {round(dt_acc * 100, 2)}%")
+print(f"Random Forest accuracy is: {round(rf_acc * 100, 2)}%")
+print(f"SVM accuracy is: {round(svm_acc * 100, 2)}%")
 
 print(classification_report(y_test, rf_pred))
