14d342c2c766cd61d51df7dc5e12cefb4514c682
linna <limsolinna@gmail.com>
2026-06-25 17:15:19 +0700

    Evaluation updated

--- evaluation.py
+++ evaluation.py
@@ -2,10 +2,12 @@ from sklearn.metrics import accuracy_score, classification_report
 from features import X_test, y_test
 from training import dt, rf, svm
 
+#Prediction
 dt_pred = dt.predict(X_test)
 rf_pred = rf.predict(X_test)
 svm_pred = svm.predict(X_test)
 
+#Calculating accuracy
 dt_acc = accuracy_score(y_test, dt_pred)
 rf_acc = accuracy_score(y_test, rf_pred)
 svm_acc = accuracy_score(y_test, svm_pred)
@@ -14,4 +16,9 @@ print(f"Decision Tree accuracy is: {round(dt_acc * 100, 2)}%")
 print(f"Random Forest accuracy is: {round(rf_acc * 100, 2)}%")
 print(f"SVM accuracy is: {round(svm_acc * 100, 2)}%")
 
+#Report
 print(classification_report(y_test, rf_pred))
+
+#Extra discussion
+print(f"The best model is Random Forest with {round(dt_acc * 100, 2)}%")
+print(f"The worst model is Decision Tree with {round(rf_acc * 100, 2)}%")
